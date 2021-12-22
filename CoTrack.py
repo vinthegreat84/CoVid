17dd48da2350d0858de9026ff5482e8496cc0375
Vinay Babu <59340330+vinthegreat84@users.noreply.github.com>
2021-12-23 02:31:29 +0530

    Add files via upload
    Citation and profile links added.

--- CoTrack.py
+++ CoTrack.py
@@ -10,7 +10,12 @@ df = pd.read_csv(url)
 
 today = date.today().strftime("%d %b, %Y")
 
-st.title("Covid Vaccination Analysis")
+st.title("Covid Vaccination Analysis - 1.0")
+st.write('## Welcome to the Covid Vaccination Analysis')
+st.write('### Created by Vinay Babu')
+st.write('''Covid Vaccination Analysis is a tool designed using Python and Streamlit to analyse covid vacinnation.''')
+
+st.sidebar.title("Covid Vaccination Analysis")
 
 if st.checkbox('Raw data as on '+today):
     df
@@ -43,3 +48,9 @@ if st.checkbox('Total vacinnations (countrywise)'):
     
     fig = px.bar(sub_df_country, x='date', y='total_vaccinations',title="Total vacinnations of " +country)
     st.plotly_chart(fig, use_container_width=True)
+
+st.sidebar.write("For vaccination dataset, check out [citation](https://www.nature.com/articles/s41562-021-01122-8)", unsafe_allow_html=True)
+    
+st.sidebar.write("For source code, check out my [github](https://github.com/vinthegreat84/CoVid)", unsafe_allow_html=True)
+
+st.sidebar.write("If you want to get in touch, you can find me on [linkedin](https://www.linkedin.com/in/vinay-babu-81791015/)", unsafe_allow_html=True)
